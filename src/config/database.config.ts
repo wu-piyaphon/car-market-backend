d1c24cebb530dd132a290f09cc1362a4e8e3c387
Piyaphon Wu <wupiyaphon.dev@gmail.com>
2025-09-01 21:24:29 +0700

    feat: simplify database configuration by using DATABASE_URL and removing individual connection parameters

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -5,12 +5,8 @@ export default registerAs(
   'database',
   (): TypeOrmModuleOptions => ({
     type: 'postgres',
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || '5432', 10),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-
+    url: process.env.DATABASE_URL,
+    ssl: process.env.SSL === 'true',
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
     migrations: [__dirname + '/../migrations/*.{ts,js}'],
     migrationsRun: process.env.NODE_ENV === 'test',

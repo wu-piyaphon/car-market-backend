bd1eeeaa305e9e544c28a7a8be6da2994aa4ba26
Piyaphon Wu <wupiyaphon.dev@gmail.com>
2025-10-04 21:37:35 +0700

    feat: add RDS CA bundle support for SSL in database configuration

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,12 +1,26 @@
+import * as fs from 'fs';
 import { registerAs } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import path from 'path';
+
+// ----------------------------------------------------------------------
+
+const RDS_CA_PATH = path.join(__dirname, '..', 'certs', 'rds-ca-bundle.pem');
+
+// ----------------------------------------------------------------------
 
 export default registerAs(
   'database',
   (): TypeOrmModuleOptions => ({
     type: 'postgres',
     url: process.env.DATABASE_URL,
-    ssl: process.env.SSL === 'true',
+    ssl:
+      process.env.SSL === 'true'
+        ? {
+            rejectUnauthorized: true,
+            ca: fs.readFileSync(RDS_CA_PATH).toString(),
+          }
+        : false,
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
     migrations: [__dirname + '/../migrations/*.{ts,js}'],
     migrationsRun: process.env.NODE_ENV === 'test',

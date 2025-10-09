8d05a152a6ed8512e0548273a2e310feaccf0b85
Piyaphon Wu <wupiyaphon.dev@gmail.com>
2025-10-09 19:58:37 +0700

    fix: update migration path resolution in data-source configuration

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -45,7 +45,7 @@ const databaseConfig = {
         }
       : false,
   entities: [path.join(__dirname, '..', '**', '*.entity{.ts,.js}')],
-  migrations: [path.join(__dirname, '..', 'migrations', '*{.ts,.js}')],
+  migrations: [path.join(__dirname, '..', '..', 'migrations', '*{.ts,.js}')],
   migrationsTableName: 'migrations',
   synchronize: false,
 };
